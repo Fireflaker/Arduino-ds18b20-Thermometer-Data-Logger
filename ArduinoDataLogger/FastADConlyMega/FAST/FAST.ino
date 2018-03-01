c24748c809cc6025b67b99d7ae9f171fb86c8d46
Fireflaker <leowzd@gmail.com>
2018-03-01 16:30:59 -0500

    Add files via upload

---
+++ ArduinoDataLogger/FastADConlyMega/FAST/FAST.ino
@@ -0,0 +1,116 @@
+
+int val0 = 0;
+int val1 = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+int val5 = 0;
+int val6 = 0;
+int val7 = 0;
+int val8 = 0;
+int val9 = 0;
+int val10 = 0;
+int val11 = 0;
+
+#define FASTADC 1
+
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+int led = 13;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 1;    // how many points to fade the LED by
+
+void setup(void)
+{
+pinMode(led, OUTPUT);
+  Serial.begin(2000000);
+ int start ;
+ int i ;
+ 
+#if FASTADC
+ // set prescale to 16
+ sbi(ADCSRA,ADPS2) ;
+ cbi(ADCSRA,ADPS1) ;
+ cbi(ADCSRA,ADPS0) ;
+#endif
+
+ Serial.begin(2000000) ;
+ Serial.print("ADCTEST: ") ;
+ start = millis() ;
+ for (i = 0 ; i < 10000 ; i++)
+analogRead(A0);
+
+ Serial.print(millis() - start) ;
+ Serial.println(" msec (10,000 calls)") ;
+
+
+
+}
+
+
+void loop(void)
+{
+  
+analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+
+  val0 = analogRead(A0);
+  val1 = analogRead(A1);
+  val2 = analogRead(A2);
+  val3 = analogRead(A3);
+  val4 = analogRead(A4);
+  val5 = analogRead(A5);
+  val6 = analogRead(A6);
+  val7 = analogRead(A7);
+  val8 = analogRead(A8);
+  val9 = analogRead(A9);
+  val10 = analogRead(A10);
+  val11 = analogRead(A11);
+
+
+
+  Serial.print(val0);
+  Serial.print(",");
+  Serial.print(val1);
+  Serial.print(",");
+  Serial.print(val2);
+  Serial.print(",");
+  Serial.print(val3);
+  Serial.print(",");
+  Serial.print(val4);
+  Serial.print(",");
+  Serial.print(val5);
+  Serial.print(",");
+  Serial.print(val6);
+  Serial.print(",");
+  Serial.print(val7);
+  Serial.print(",");
+  Serial.print(val8);
+  Serial.print(",");
+  Serial.print(val9);
+  Serial.print(",");
+  Serial.print(val10);
+  Serial.print(",");
+  Serial.println(val11);
+
+
+
+}
+
+
+
+
+
---
+++ ArduinoDataLogger/UNOTEMP_Display/TEMP_Display/TEMP_Display.ino
@@ -0,0 +1,179 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+#include <LiquidCrystal.h>
+
+#include "DHT.h"
+#define DHTPIN 3
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+float temp1 = 88;
+float temp2 = 88;
+float h = 66;
+float t = 66;
+
+int val0 = 0;
+int val1 = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+int val5 = 0;
+
+long count = 0;
+int displayVal = -10;
+
+
+//#define TEMP_9_BIT  0x1F //  9 bit
+//#define TEMP_10_BIT 0x3F // 10 bit
+//#define TEMP_11_BIT 0x5F // 11 bit
+#define TEMP_12_BIT 0x7F // 12 bit
+
+void setup(void)
+{
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // start serial port
+  Serial.begin(115200);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  dht.begin();
+  // Start up the library
+  sensors.begin();
+  lcd.clear();
+
+}
+
+
+void loop(void)
+{
+
+  val0 = analogRead(A0);
+  val1 = analogRead(A1);
+  val2 = analogRead(A2);
+  val3 = analogRead(A3);
+  val4 = analogRead(A4);
+  val5 = analogRead(A5);
+  
+h = dht.readHumidity();
+f = dht.readTemperature() == Celcius;
+
+
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  //  Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  //  Serial.println("DONE");
+
+  temp1 = sensors.getTempCByIndex(0);
+  temp2 = sensors.getTempCByIndex(1);
+  // Why "byIndex"?
+  // You can have more than one IC on the same bus.
+  // 0 refers to the first IC on the wire
+
+
+
+  Serial.print(count);
+  Serial.print("  No.1 Temp: ");
+  Serial.print(temp1);
+  Serial.print("  No.2 Temp: ");
+  Serial.print(temp2);
+  Serial.print("  v0: ");
+  Serial.print(val0);
+  Serial.print("  v1: ");
+  Serial.print(val1);
+  Serial.print("  v2: ");
+  Serial.print(val2);
+  Serial.print("  v3: ");
+  Serial.print(val3);
+  Serial.print("  v4: ");
+  Serial.print(val4);
+  Serial.print("  v5: ");
+  Serial.println(val5);
+
+
+
+  // set the cursor to column 0, line 0
+  // (note: line 1 is the second row, since counting begins with 0):
+  if (count % 20 == 0)
+  {
+    lcd.clear();
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("A:");
+  lcd.print(temp1);
+
+  lcd.setCursor(8, 0);
+  lcd.print("B:");
+  lcd.print(temp2);
+
+  lcd.setCursor(0, 1);
+  lcd.print(count);
+
+  count++;
+  displayVal++;
+
+  if (displayVal > 5) {
+    displayVal = 0;
+  }
+
+  //Serial.println(displayVal);
+
+//  lcd.setCursor(7, 1);
+//  lcd.print("Val");
+  lcd.setCursor(7, 1);
+  lcd.print(displayVal);
+  lcd.print(":");
+
+
+
+  if (displayVal == 0) {
+    lcd.print(val0);
+  }
+  if (displayVal == 1) {
+    lcd.print(val1);
+  }
+  if (displayVal == 2) {
+    lcd.print(val2);
+  }
+  if (displayVal == 3) {
+    lcd.print(val3);
+  }
+  if (displayVal == 4) {
+    lcd.print(val4);
+  }
+  if (displayVal == 5) {
+    lcd.print(val5);
+  }
+
+  if (isnan(h) || isnan(f)) {
+    lcd.print("ERROR");
+    return;
+  }
+
+  lcd.setCursor(7, 1);
+  lcd.print(f);
+  lcd.setCursor(7,1);
+  lcd.print(h);  
+
+}
+
+
+
+
+
